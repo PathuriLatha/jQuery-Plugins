5c1df28aea24a488c370ca35582dcfb358098323
saikumarpuli <saikumarpuli9@gmail.com>
2019-12-20 12:41:45 +0530

    initial commit

---
+++ website/script.js
@@ -0,0 +1,6 @@
+$(function(){
+  $("#divBody").css({
+    "background-image" : "url(./images/body.jpg)",
+    "background-size" : "50%",
+  });
+});
---
+++ website/side-bar.js
@@ -0,0 +1,16 @@
+$(function(){
+  $("#divBody").css({
+    //"background-image" : "url(./images/body.jpg)",
+    //"background-size" : "50%",
+    "height" : "300px",
+    "width" : "1070px",
+    "padding" : "10px",
+    "margin" : "0px auto"
+  });
+  $("#footer").css({
+    "height" : "300px",
+    "width" : "1070px",
+    "padding" : "10px",
+    "margin" : "0px auto"
+  })
+});
