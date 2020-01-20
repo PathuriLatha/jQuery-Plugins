131cf43edb2bbd97e43de324ad86cef81db15895
saikumarpuli <saikumarpuli9@gmail.com>
2020-01-20 18:07:32 +0530

    jQuery UI widgets

---
+++ jQuery-UI-widgets/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+  //alert("hello..!!");
+  $("#projects").tabs();
+
+  $("ul").sortable({
+    axis: "x",
+    containment: "#projects"
+  });
+
+  $("ol").sortable({
+    axis: "y",
+    containment: "#projects"
+  });
+
+  $("#btnAddTask").button().click(
+    function(){
+      $("#task-dialog").dialog({
+        width: 400,
+        resizable: false,
+        modal: true,
+        buttons: {
+          "Add new task": function(){
+            $("#projects").tabs("refresh");
+            var activeTab = $("#projects").tabs("option", "active");
+            var title = $("#main > li:nth-child(1) > a").attr(href)
+            alert(activeTab);
+            $("#new-task").val("");
+            $(this).dialog("close");
+          },
+          "cancel": function(){
+            $("#new-task").val("");
+            $(this).dialog("close");
+          }
+        }
+      })
+    }
+  );
+
+  $("#btnAddProject").button().click(
+    function(){
+      $("#project-dialog").dialog({
+        width: 400,
+        resizable: false,
+        modal: true,
+        buttons: {
+          "Add new project": function(){
+            var projectName = $("#new-project").val();
+            $("<li><a href='#" +projectName+ "'>" +projectName+ "</a></li>").appendTo("#main");
+            $("<ol id='" +projectName+"'></ol>").appendTo("#projects");
+            $("#projects").tabs("refresh");
+            var tabCount = $("#projects .ui-tabs-nav li").length;
+            $("#projects").tabs("option", "active", tabCount-1);
+            $("#new-project").val("");
+            $(this).dialog("close");
+          },
+          "cancel": function(){
+            $("#new-project").val("");
+            $(this).dialog("close");
+          }
+        }
+      });
+    }
+  );
+
+});
