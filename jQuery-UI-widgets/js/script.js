44290fb662259297b04b8ad6331634bd08859295
saikumarpuli <saikumarpuli9@gmail.com>
2020-01-27 11:45:37 +0530

    removing projects and tasks

--- jQuery-UI-widgets/js/script.js
+++ jQuery-UI-widgets/js/script.js
@@ -12,6 +12,39 @@ $(document).ready(function(){
     containment: "#projects"
   });
 
+  $("#projects").on("click", "input[type=checkbox]", function(){
+      if(!alert("Are you want to remove this one")){
+        $(this).closest("li").slideUp(function(){
+          //$(this).dialog("dialog");
+          $(this).remove();
+        });
+      }
+    }
+    /*
+    $("#showPrompt").simpleConfirm({
+      message: "Are you sure remove ?",
+      success: function(){
+        $(this).closest("li").slideUp(function(){
+          $(this).dialog("dialog");
+          $(this).remove();
+        });
+      },
+      cancel: function(){
+        alert("You canceled this one");
+      }
+    });
+    }
+    */
+  );
+
+  $("#projects").on("click", "span.ui-icon-close", function(){
+    var index = $(this).closest("li").index();
+    var id = $("#main li:eq(" +index+ ") a").attr("href");
+    $("#main li:eq(" +index+ ")").remove();
+    $(id).remove();
+    $("#projects").tabs("refresh");
+  });
+
   $("#btnAddTask").button().click(
     function(){
       $("#task-dialog").dialog({
@@ -20,10 +53,13 @@ $(document).ready(function(){
         modal: true,
         buttons: {
           "Add new task": function(){
+            //get active tab values
             $("#projects").tabs("refresh");
             var activeTab = $("#projects").tabs("option", "active");
-            var title = $("#main > li:nth-child(1) > a").attr(href)
-            alert(activeTab);
+            var title = $("#main > li:nth-child("+(activeTab+1)+") > a").attr("href");
+            $("#projects "+title).append("<li><input type='checkbox'>"+$("#new-task").val()+"</li>");
+            $("#new-task").val("")
+            // alert(activeTab);
             $("#new-task").val("");
             $(this).dialog("close");
           },
@@ -45,8 +81,9 @@ $(document).ready(function(){
         buttons: {
           "Add new project": function(){
             var projectName = $("#new-project").val();
-            $("<li><a href='#" +projectName+ "'>" +projectName+ "</a></li>").appendTo("#main");
-            $("<ol id='" +projectName+"'></ol>").appendTo("#projects");
+            var replaceName = projectName.split(" ").join("_");
+            $("<li><a href='#" +replaceName+ "'>" +projectName+ "</a><span class='ui-icon ui-icon-close'></span></li>").appendTo("#main");
+            $("<ol id='" +replaceName+"'></ol>").appendTo("#projects").sortable();
             $("#projects").tabs("refresh");
             var tabCount = $("#projects .ui-tabs-nav li").length;
             $("#projects").tabs("option", "active", tabCount-1);
